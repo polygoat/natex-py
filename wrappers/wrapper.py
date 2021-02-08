904140e4262158999c2f609fb5b63ac9524e76a1
Dan Borufka <danborufka@gmail.com>
2021-02-08 13:02:11 +0100

    Fixing parsing of nested properties and hence adding support for SPAN property

--- wrappers/wrapper.py
+++ wrappers/wrapper.py
@@ -1,10 +1,14 @@
 import pydash as _
 
-class ParsedSentence(list): pass
-class ParsedToken: pass
+class ParsedSentence(list): 
+	pass
+
+class ParsedToken: 
+	def __repr__(self):
+		return repr(self.__dict__)
 
 class Wrapper:
-	SUPPORTED_KEYS = ['literal', 'lemma', 'upos', 'xpos', 'udep', 'ner', 'features']
+	SUPPORTED_KEYS = ['literal', 'lemma', 'upos', 'xpos', 'udep', 'ner', 'span', 'features']
 	SPLIT_FEATURES = True
 
 	needs_setup = False
@@ -26,29 +30,59 @@ class Wrapper:
 	def get_tokens(self, parsed_sentence):
 		return parsed_sentence
 
-	def preprocess(self, tag, key):
+	def preprocess(self, value, key):
+		if isinstance(value, dict):
+			for item, entry in value.items():
+				value[item] = self.preprocess(entry, item)
+			return value
+		elif isinstance(value, list):
+			return list(map(lambda item: self.preprocess(item, key), value))
+
 		if key not in ['literal', 'lemma']:
-			return str(tag).upper()	
-		return tag
+			if value.isnumeric():
+				return float(value)
+			return str(value).upper()	
+		return value
 
 	def process(self, sentence, language_code='en'):
 		if hasattr(self, 'load_processor'):
 			nlp = self.load_processor()
 			sentence = nlp(sentence)
 
+	@staticmethod
+	def __parse_features(feature_string):
+		if isinstance(feature_string, str):
+			if '=' in feature_string:
+				return Wrapper.__split_features(feature_string)
+		return feature_string
+
 	def map(self, item):
 		index, token = item
 		parsed_token = ParsedToken()
 		for key in Wrapper.SUPPORTED_KEYS:
-			prop = _.get(self.KEY_MAPPING, key, key)
-			value = _.get(token, prop, '')
-			value = self.preprocess(value, key)
+			props = _.get(self.KEY_MAPPING, key, key)
 
-			if key == 'features':
-				if self.SPLIT_FEATURES:
-					value = Wrapper.__split_features(value)
+			if not isinstance(props, list):
+				props = [props]
 
-			setattr(parsed_token, key, value)
+			values = []
+
+			for prop in props:
+				if '.' in prop:
+					container_prop = prop.split('.')[0]
+					container_value = _.get(token, container_prop, '')
+					container_value = Wrapper.__parse_features(container_value)
+					_.set_(token, container_prop, container_value)
+
+				value = _.get(token, prop, '')
+				value = Wrapper.__parse_features(value)
+				value = self.preprocess(value, key)
+				values.append(value)
+
+			if len(values) == 1:
+				values = values[0]
+
+			_.set_(parsed_token, key, values)
 		parsed_token.index = index
 		return parsed_token
 
